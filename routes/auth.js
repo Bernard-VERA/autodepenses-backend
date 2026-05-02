f1ddd320d1fa86653c0a74bf9fd052eefff9a97f
Bernard-VERA <bvmars@hotmail.com>
2026-05-02 22:49:30 +0200

    Retour vers auth/verify au lieu de auth/callback

--- routes/auth.js
+++ routes/auth.js
@@ -47,7 +47,7 @@ router.post("/send-magic-link",
             await user.save();
 
             // Construire le lien
-            const magicLink = `${process.env.FRONTEND_URL}/auth/callback?token=${magicToken}&email=${encodeURIComponent(user.email)}`;
+            const magicLink = `${process.env.FRONTEND_URL}/auth/verify?token=${magicToken}&email=${encodeURIComponent(user.email)}`;
 
 
             // Envoyer l'email
